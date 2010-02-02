5cd75b7c1558da02369d142b79bd4aca52206767
Casey Dreier <cdreier@gmail.com>
2010-02-01 20:37:48 -0500

    Adding additional test for outgoing email with address array.

--- test/deliver_to_developer_test.rb
+++ test/deliver_to_developer_test.rb
@@ -7,37 +7,52 @@ end
 
 class DeliverToDeveloperTest < ActionMailer::TestCase
   
-  test "intended delivery method returns the proper delivery types" do
-    assert @expected.respond_to? :intended_delivery_method
-  end
-  
   test "that the email address is changed properly when delivery method is appended with 'to_developer'" do
     
-    DeliverToDeveloper.developer_emails = %w(cjd47@sha.cornell.edu)
+    DeliverToDeveloper.developer_emails = 'casey@casey.com'
 
-    # Make a mock object of the request object for the ApplicationController
-    # It only needs to respond like the subdomain call does, with an array of subdomains.
-    ap = ApplicationController.new
-    ap.request = mock()
-    
-    @expected.from    = 'test@test.com'
-    @expected.to      = 'original@receiver.com'
-    @expected.subject = 'Test Subject'
-    @expected.body    = 'blah'
-    @expected.date    = Time.now
+    # Setup our test email
+    prepare_test_email
   
     # Append with "to_developer" to trigger the code #
     ActionMailer::Base.delivery_method = :test_to_developer
     ActionMailer::Base.deliver(@expected)
 
     # Make sure something was sent #
-    assert ActionMailer::Base.deliveries.size > 0
+    assert ActionMailer::Base.deliveries.size == 1
     
     # Make sure that the email was changed the the netid's SHA email address #
-    assert_equal 'cjd47@sha.cornell.edu',ActionMailer::Base.deliveries.first.to.first
+    assert_equal 'casey@casey.com',ActionMailer::Base.deliveries.first.to.first
+    
+    # Make sure the "from" field was unchanged #
+    assert_equal 'test@test.com',ActionMailer::Base.deliveries.first.from.first
+  end
+  
+  test "that the developer_emails setting will accept an array of email addresses" do
+    prepare_test_email
+    DeliverToDeveloper.developer_emails = %w(casey@casey.com huminahowwa@casey.com)
+    
+    # Append with "to_developer" to trigger the code #
+    ActionMailer::Base.delivery_method = :test_to_developer
+    ActionMailer::Base.deliver(@expected)
+
+    # Make one email was sent #
+    assert ActionMailer::Base.deliveries.size == 1
+    
+    # Make sure that the outgoing email's "to" address was changed properly #
+    assert_equal ['casey@casey.com','huminahowwa@casey.com'].sort,ActionMailer::Base.deliveries.first.to.sort
     
     # Make sure the "from" field was unchanged #
     assert_equal 'test@test.com',ActionMailer::Base.deliveries.first.from.first
     
+  end
+  
+  def prepare_test_email
+    # Setup our test email
+    @expected.from    = 'test@test.com'
+    @expected.to      = 'original@receiver.com'
+    @expected.subject = 'Test Subject'
+    @expected.body    = 'blah'
+    @expected.date    = Time.now
   end
 end
